e97e92818d882e310c1a4837f8e56d4a7f7ac4bf
Chance Lane <lanecw@gmail.com>
2021-01-28 14:27:08 -0500

    added sorting capability by officer and conviction. Revisit flow of writer function for smoother performance

--- scripts/universal/htmlObject.js
+++ scripts/universal/htmlObject.js
@@ -1,40 +1,20 @@
 //parameters: current(defined by writer for iteration)
 //            category(defined by writer at link click)
 //            ONLY needed at criminal category. Defined by dropdown change. Used to determine which elements to filter
-export const htmlObject = (current,category,conviction) => {
+export const htmlObject = (current,category) => {
     if (category == 'criminal') {
         //if default is selected at dropdown;print all criminals. false failed attempt to catch at page load, reevaluate
-        if (conviction === '0' || conviction === false){
-            console.log('here1')
-            console.log(current.name)
-            return `
-                <li> Name: ${current.name}</li>
-                <li> Age: ${current.age}</li>
-                <li> Incarceration Date: ${new Date(current.incarceration.start).toLocaleDateString('en-US')}</li>
-                <li> Release Date: ${new Date(current.incarceration.end).toLocaleDateString('en-US')}</li>
-                <li> Address: ${current.address}</li>
-                <li> Arresting Officer: ${current.arrestingOfficer}</li>
-                <li> Conviction: ${current.conviction}</li>
-                <br>
-            `
-        //if conviction in array matches user selected conviction at dropdown include at filter
-        }else if (conviction === current.conviction){
-            console.log('here2')
-            console.log(current.name)
-            return `
-                <li> Name: ${current.name}</li>
-                <li> Age: ${current.age}</li>
-                <li> Incarceration Date: ${new Date(current.incarceration.start).toLocaleDateString('en-US')}</li>
-                <li> Release Date: ${new Date(current.incarceration.end).toLocaleDateString('en-US')}</li>
-                <li> Address: ${current.address}</li>
-                <li> Arresting Officer: ${current.arrestingOfficer}</li>
-                <li> Conviction: ${current.conviction}</li>
-                <br>
-            `
-        //if user selected conviction doesnt match criminals.conviction, return an empty string to writer. If not 'undefined' will be printed
-        }else{
-            return '';
-        }
+        
+        return `
+            <li> Name: ${current.name}</li>
+            <li> Age: ${current.age}</li>
+            <li> Incarceration Date: ${new Date(current.incarceration.start).toLocaleDateString('en-US')}</li>
+            <li> Release Date: ${new Date(current.incarceration.end).toLocaleDateString('en-US')}</li>
+            <li> Address: ${current.address}</li>
+            <li> Arresting Officer: ${current.arrestingOfficer}</li>
+            <li> Conviction: ${current.conviction}</li>
+            <br>
+        `
     }else if (category === 'officer'){
         console.log('here')
         console.log(current.name)
